40214f871e113e699baddab6b1eb3a294edbd1f0
Sindre D Løken <sindredl@msn.com>
2017-08-27 21:01:26 +0200

    firebase setup done

---
+++ SindredlGram/Controller/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SindredlGram
+//
+//  Created by Sindre Dahl Løken on 27.08.2017.
+//  Copyright © 2017 Sindre Dahl Løken. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
