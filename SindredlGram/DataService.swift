875b379d5c346019b92fa5fe41feda870989bab5
Sindre D Løken <sindredl@msn.com>
2017-08-30 14:38:57 +0200

    Finished func to display images  from fb storage in the ui and caching of images.

--- SindredlGram/DataService.swift
+++ SindredlGram/DataService.swift
@@ -12,14 +12,20 @@ import Foundation
 import Firebase
 
 let DB_BASE = Database.database().reference()
+let STORAGE_BASE = Storage.storage().reference()
+
 class DataService {
     
     //Singleton - Instanse av en klasse som er globalt tilgjengelig - kun en instance
     static let ds = DataService()
     
+    //DB References
     private var _REF_BASE = DB_BASE
     private var _REF_POSTS = DB_BASE.child("posts")
     private var _REF_USERS = DB_BASE.child("users")
+    
+    //Storage references
+    private var _REF_POST_IMAGES = STORAGE_BASE.child("post-pics")
     
     var REF_BASE: DatabaseReference {
         return _REF_BASE
@@ -32,6 +38,11 @@ class DataService {
     var REF_USERS: DatabaseReference {
         return _REF_USERS
     }
+    
+    var REF_POST_IMAGES: StorageReference {
+        return _REF_POST_IMAGES
+    }
+    
     
     func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
         REF_USERS.child(uid).updateChildValues(userData)
--- SindredlGram/View/PostCell.swift
+++ SindredlGram/View/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
     
@@ -23,10 +24,32 @@ class PostCell: UITableViewCell {
         // Initialization code
     }
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage? = nil) {
         self.post = post
         self.caption.text = post.caption
         self.likesLbl.text = "\(post.likes)"
+        
+        if img != nil {
+            //Image is in cache
+            self.postImg.image = img
+        } else {
+            //Image not in cahce - download it
+            let ref = Storage.storage().reference(forURL: post.imageUrl)
+            ref.getData(maxSize: 2 * 1024 * 1024, completion: { (data, error) in
+                if error != nil {
+                    print("SINDRE: Kunne ikke laste ned bilde fra Firebase Storage...")
+                } else {
+                    print("SINDRE: Bilde lastet ned fra Firebase storage vellykket!")
+                    if let imgData = data {
+                        if let img = UIImage(data: imgData) {
+                            self.postImg.image = img
+                            FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                        }
+                    }
+                }
+                
+            })
+        }
     }
     
     
